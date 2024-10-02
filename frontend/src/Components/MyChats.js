b34cf11faa853b004523150e7dc36e54babea751
G-irish <123972157+G-irish@users.noreply.github.com>
2024-10-02 12:19:23 +0530

    final features added with latest message preview

--- frontend/src/Components/MyChats.js
+++ frontend/src/Components/MyChats.js
@@ -52,6 +52,12 @@ const MyChats = ({fetchAgain}) => {
                 {!chat.isGroupChat && chat.users && loggedUser ?getSender(loggedUser,chat.users):
                 chat.chatName}
               </Text>
+              {chat.latestMessage && (
+                <Text fontSize="xs">
+                  <b>{chat.latestMessage.sender.name} :</b>
+                  {chat.latestMessage.content.length>50 ? chat.latestMessage.content.substring(0,51)+"..." : chat.latestMessage.content}
+                </Text>
+              )}
             </Box>  
           ))}
         </Stack>
