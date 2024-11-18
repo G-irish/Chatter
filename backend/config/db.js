100d19290830a329c2b7d29d9da77a069f43037d
G-irish <123972157+G-irish@users.noreply.github.com>
2024-11-18 20:12:09 +0530

    node modules removed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,8 @@
 const mongoose =require('mongoose');
 async function connectDB(){
+    console.log(process.env.BACKEND_PORT);
     try{
         const con=await mongoose.connect(process.env.MONGO_CONNECTION_URL,{useNewUrlParser:true ,useUnifiedTopology:true});
-        
-        // console.log(process.env.APP_BASE_URL);
         console.log(`Database Connected : ${con.connection.host}`.cyan.underline);
 
     }catch(err){
